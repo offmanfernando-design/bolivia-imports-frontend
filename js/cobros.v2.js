245fb39c34992db1569597ae4ad344405e21a393
offmanfernando-design <offman.fernando@gmail.com>
2026-02-19 17:10:34 -0400

    cobros.v2.js

--- js/cobros.v2.js
+++ js/cobros.v2.js
@@ -339,6 +339,8 @@ window.avisar = async function (clienteId, telefono) {
    ========================= */
 
 async function generarMensaje(clienteId) {
+   console.log(productos);
+
   const res = await fetch(`${API_BASE_URL}/api/cobros/detalle/${clienteId}`);
   const productos = await res.json();
   if (!productos.length) return '';
